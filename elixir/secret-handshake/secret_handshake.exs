bd46ac9432819925df5bc3fe359fbb1f77ae0928
Kacper Golinski <kacper.golinski@gmail.com>
2017-09-26 01:05:35 +0200

    use list comprehension in elixir/secret-handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,21 +1,14 @@
 defmodule SecretHandshake do
   use Bitwise, only_operators: true
 
-  @flag_reverse 16
+  @flag_reverse 0b10000
 
-  @action_flags [
-    1,
-    2,
-    4,
-    8,
+  @actions [
+    {0b00001, "wink"},
+    {0b00010, "double blink"},
+    {0b00100, "close your eyes"},
+    {0b01000, "jump"},
   ]
-
-  @actions %{
-    1 => "wink",
-    2 => "double blink",
-    4 => "close your eyes",
-    8 => "jump",
-  }
 
   @doc """
   Determine the actions of a secret handshake based on the binary
@@ -33,18 +26,16 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
-    @action_flags
-    |> Enum.map(fn(flag) -> map_code_to_action(code, flag) end)
-    |> Enum.filter(fn(action) -> !is_nil(action) end)
+    code
+    |> find_actions()
     |> maybe_reverse(flag_is_on?(code, @flag_reverse))
   end
 
-  @spec map_code_to_action(integer, integer) :: String.t | nil
-  defp map_code_to_action(code, flag) do
-    case flag_is_on?(code, flag) do
-      true  -> @actions[flag]
-      false -> nil
-    end
+  @spec commands(code :: integer) :: list(String.t())
+  defp find_actions(code) do
+    for {flag, action} <- @actions,
+      flag_is_on?(code, flag),
+      do: action
   end
 
   @spec flag_is_on?(integer, integer) :: boolean
@@ -53,7 +44,7 @@ defmodule SecretHandshake do
   end
 
   @spec maybe_reverse(list(String.t()), boolean) :: list(String.t())
-  defp maybe_reverse(actions, false), do: actions
-  defp maybe_reverse(actions, true),  do: Enum.reverse(actions)
+  defp maybe_reverse(actions, false),      do: actions
+  defp maybe_reverse(actions, _reverse?),  do: Enum.reverse(actions)
 end
 
